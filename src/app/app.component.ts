65474c722f7afe066fbc012240948046dfd2efd9
kfeldmer <karl.feldmer@gmail.com>
2023-02-23 17:29:28 +0100

    Alan AI integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-forwarder-suite-template';
+  alanBtnInstance;
+  
+  constructor(private router: Router){
+    this.alanBtnInstance = alanBtn({
+      key: '2ec5b4d0f7c942fe209c255d41bee8d62e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: (commandData: any) => {
+        if (commandData.command === 'navigation' && commandData.route) {
+          this.router.navigateByUrl(commandData.route);
+          console.log(commandData.route);
+        }
+      },
+    });
+  }
 }
